9ef891eec960922cad22833f6a5de60aa9e2b8be
Verbiush <jupacan07@gmail.com>
2024-03-12 00:10:03 -0500

    ejercicio interface

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
