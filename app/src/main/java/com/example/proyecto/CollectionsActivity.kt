da4fa7bad8095433c5fc7f1ac9062042df1962db
Verbiush <jupacan07@gmail.com>
2024-03-18 23:55:07 -0500

    ejercicios if

--- app/src/main/java/com/example/proyecto/CollectionsActivity.kt
+++ app/src/main/java/com/example/proyecto/CollectionsActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyecto.model.Country
+import kotlin.time.times
+
 
 class CollectionsActivity : AppCompatActivity() {
 
@@ -13,15 +15,14 @@ class CollectionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_collections)
 
         val countries = listOf("Germany", "India", "Japan", "Brazil", "Australia")
-        val printIndex= searchCountry(countries,"España")
-        Log.d("busqueda",printIndex.toString())
-        val country = Country("Colombia","America")
-        val grecia = Country("Grecia","europa")
-        val countriesClass = listOf(country,grecia)
-        for(tempCountry in countriesClass){
-            Log.d("addCountry",tempCountry.countryInformation())
+        val printIndex = searchCountry(countries, "India")
+        Log.d("busqueda", printIndex.toString())
+        val country = Country("Colombia", "America")
+        val grecia = Country("Grecia", "europa")
+        val countriesClass = listOf(country, grecia)
+        for (tempCountry in countriesClass) {
+            Log.d("addCountry", tempCountry.countryInformation())
         }
-
 
 
         val cities = mutableListOf("Berlin", "Calcutta", "Seoul", "Sao Paulo", "Sydney")
@@ -40,43 +41,109 @@ class CollectionsActivity : AppCompatActivity() {
         printPerson(hashmap)
 
         val array = arrayOfNulls<String>(100)
-        array[1]="perros"
-        array.set(3,"gatos")
+        array[1] = "perros"
+        array.set(3, "gatos")
 
-        val arrayDos = arrayOf(1,4,"cuatro")
-        for (number in arrayDos){
-            Log.d("number",number.toString())
+        val arrayDos = arrayOf(1, 4, "cuatro")
+        for (number in arrayDos) {
+            Log.d("number", number.toString())
         }
 
-        for(string in array){
-            Log.d("vector",string.toString())
-        }
-    }
-
-    private fun listprint(list: List<String>) {
-        for (item in list) {
-            Log.d("lists", item)
-        }
-    }
-
-    private fun searchCountry(list: List<String>, searchCountry:String) :Int {
-
-        for (index in 0 until list.size) {
-            var countryTemp = list[index]
-            if (countryTemp==searchCountry){
-               return index
-            }
-        }
-        return -1
-    }
-    private fun printPerson(hashMap: HashMap<Int, String>) {
-        hashMap.forEach { item ->
-            var key = item.key
-            var value = item.value
-            Log.d("key valido", key.toString() + "--" + value)
+        for (string in array) {
+            Log.d("vector", string.toString())
         }
 
+        val numbers = listOf(2, 3, 13, 4, 12, 6, 7, 8, 9, 10)
+        conditionNumbers(numbers)
+
+        printMayorMenorQue100(5)
+
+
+
+      printResultadoOperacion(5,3)
 
     }
 
 }
+
+
+private fun printResultadoOperacion(numberUno:Int,numberDos:Int){
+
+
+
+    if (numberUno < numberDos) {
+        var resultadoSuma= numberUno+numberDos
+        Log.d("Operador Suma",resultadoSuma.toString())
+
+
+    } else {
+        var resultadoMulti= numberUno*numberDos
+        Log.d("operador Multiplicacion",resultadoMulti.toString())
+
+
+    }
+
+
+}
+private fun printMayorMenorQue100(number: Int){
+
+    if (number<100){
+        Log.d("Menor",number.toString())
+
+    }else if (number>100){
+        Log.d("Mayor",number.toString())
+    }else if(number==100){
+        Log.d("igual",number.toString())
+    }
+    if(number >1 && number < 10){
+        Log.d("mayorQue&&MenorQue",number.toString())
+    }
+}
+ private fun conditionNumbers(list: List<Int>) {
+
+    for (index in 0 until list.size) {
+     var numbersTemp=list[index]
+
+        if (numbersTemp %2 == 0) {
+
+            Log.d(
+                "ListNumbers",
+                " el numero" + " " + numbersTemp + " " + "Es par" + " " + "el index es" + " " + index
+            )
+        } else  {
+            Log.d(
+                "ListNumbers",
+                " el numero" + " " + numbersTemp + " " + "Es impar" + " " + "el index es" + " " + index
+            )
+        }
+    }
+}
+
+private fun listprint(list: List<String>) {
+    for (item in list) {
+        Log.d("lists", item)
+    }
+}
+
+
+private fun searchCountry(list: List<String>, searchCountry: String): Int {
+
+    for (index in 0 until list.size) {
+        var countryTemp = list[index]
+        if (countryTemp == searchCountry) {
+            return index
+        }
+    }
+    return -1
+}
+
+private fun printPerson(hashMap: HashMap<Int, String>) {
+    hashMap.forEach { item ->
+        var key = item.key
+        var value = item.value
+        Log.d("key valido", key.toString() + "--" + value)
+    }
+
+
+}
+
--- app/src/main/java/com/example/proyecto/ContactAdapter.kt
+++ app/src/main/java/com/example/proyecto/ContactAdapter.kt
@@ -15,7 +15,7 @@ import com.squareup.picasso.Picasso
 class ContactAdapter(private val contactList: List<Contact>) :
     RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
 
-    private var onContactClickListener: OnContactClickListener?=null
+    private var onContactClickListener: OnContactClickListener? = null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_contact, parent, false)
@@ -26,35 +26,37 @@ class ContactAdapter(private val contactList: List<Contact>) :
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
 
         val contact = contactList[position]
-        holder.chatTime.text=contact.contactTime
-        holder.userName.text=contact.contactName
-        holder.chatPreview.text=contact.previewChatContact
+        holder.chatTime.text = contact.contactTime
+        holder.userName.text = contact.contactName
+        holder.chatPreview.text = contact.previewChatContact
 
-        if (contact.imageContact.isNotEmpty()){
+        if (contact.imageContact.isNotEmpty()) {
             Picasso.with(holder.itemView.context)
                 .load(contact.imageContact).into(holder.profileImage)
-        }else{
+        } else {
             Picasso.with(holder.itemView.context)
-                .load("https://cdn.pixabay.com/photo/2017/11/10/05/48/user-2935527_640.png").into(holder.profileImage)
+                .load("https://cdn.pixabay.com/photo/2017/11/10/05/48/user-2935527_640.png")
+                .into(holder.profileImage)
         }
 
         holder.itemContactContainer.setOnClickListener {
         }
         holder.itemView.setOnClickListener {
-            onContactClickListener?.onContactClick(position,contact)
+            onContactClickListener?.onContactClick(position, contact)
         }
 
     }
+
     override fun getItemCount(): Int {
         return contactList.size
     }
 
     class ContactViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-        val itemContactContainer: View=itemView.findViewById(R.id.itemContactContainer)
-        val chatTime:TextView=itemView.findViewById(R.id.chatTime)
-        val userName:TextView=itemView.findViewById(R.id.userName)
-        val chatPreview:TextView=itemView.findViewById(R.id.chatPreview)
-        val profileImage:ImageView=itemView.findViewById(R.id.profileImage)
+        val itemContactContainer: View = itemView.findViewById(R.id.itemContactContainer)
+        val chatTime: TextView = itemView.findViewById(R.id.chatTime)
+        val userName: TextView = itemView.findViewById(R.id.userName)
+        val chatPreview: TextView = itemView.findViewById(R.id.chatPreview)
+        val profileImage: ImageView = itemView.findViewById(R.id.profileImage)
 
     }
 
